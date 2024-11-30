4f86ee61bb461d3ab5a826c496f0e19e2c70dfb1
Sathishg090603 <sathishg090603@gmail.com>
2024-11-30 17:25:12 +0530

    created two java files

---
+++ java/loop.java
@@ -0,0 +1,10 @@
+package Repo2.java;
+
+public class loop {
+     public static void main(String[] args) {
+        for(int i=0;i<=5;i++)
+        {
+            System.err.println(i);
+        }
+     }
+}
