1fe053d8cf814a0a5ec5dcfacd8ac4f746392c37
Sathishg090603 <sathishg090603@gmail.com>
2024-11-30 16:59:10 +0530

    used % operator instead & operator

--- oddOrEven.java
+++ oddOrEven.java
@@ -2,9 +2,15 @@ public class oddOrEven{
     public static void main(String args[])
     {
         int num=10;
-        if((num & 1)==0)
+       /*  if((num & 1)==0)
         System.out.println("num is even");
         else
-        System.err.println("num is odd");
+        System.err.println("num is odd");*/
+
+        if(num%2==0)
+        System.err.println("even");
+        else
+        System.err.println("odd");
     }
+
 }
