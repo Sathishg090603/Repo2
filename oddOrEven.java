1419497d34a1f055f6d1074afdfd48f14a269471
Sathishg090603 <sathishg090603@gmail.com>
2024-11-30 16:52:52 +0530

    both source and class files created for oddoreven program

---
+++ oddOrEven.java
@@ -0,0 +1,10 @@
+public class oddOrEven{
+    public static void main(String args[])
+    {
+        int num=10;
+        if((num & 1)==0)
+        System.out.println("num is even");
+        else
+        System.err.println("num is odd");
+    }
+}
